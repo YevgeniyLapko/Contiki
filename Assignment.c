22dc5f31e26b7b57ecc61aac4009a472980f974e
Yevgeniy Lapko <32012302+YevgeniyLapko@users.noreply.github.com>
2017-11-13 12:09:29 +0000

    Add files via upload

--- Assignment.c
+++ Assignment.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "net/rime.h"
 #include "random.h"
+#include "dev/sht11-sensor.h"
 
     
     PROCESS(example_broadcast_process, "Broadcast example");
@@ -11,6 +12,12 @@ AUTOSTART_PROCESSES(&example_broadcast_process);
 /*---------------------------------------------------------------------------*/
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
 static struct broadcast_conn broadcast;
+/*---------------------------------------------------------------------------*/
+//static int;
+get_temp(void)
+{
+  return ((sht11_sensor.value(SHT11_SENSOR_TEMP) / 10) - 396) / 10;
+}
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
@@ -25,11 +32,14 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
 
   while(1) {
 
-    /* Delay 2-4 seconds */
-   // etimer_set(&et, CLOCK_SECOND * 4 + random_rand() % (CLOCK_SECOND * 4));
+    /* Delay 4 seconds */
+   etimer_set(&et, CLOCK_SECOND * 4);
+   SENSORS_ACTIVATE(sht11_sensor);
+   
+   PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    PROCESS_WAIT_EVENT_UNTIL((ev==sensors_event) && (data == &button_sensor));
-
+    //PROCESS_WAIT_EVENT_UNTIL((ev==sensors_event) && (data == &button_sensor));
+	printf("Temperature: %d \n", get_temp());
     packetbuf_copyfrom("Eugen", 6);
     broadcast_send(&broadcast);
     printf("broadcast message sent\n");
